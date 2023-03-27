2508cb61f9c6a3ac55a1963b2659eab5e889c5f2
valentin-CARIA <79085983+valentin-CARIA@users.noreply.github.com>
2023-03-27 16:20:39 +0200

    Add files via upload

---
+++ src/Form/FormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+use App\Entity\MessUtilisateur;
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class FormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom_cont')
+            ->add('prenom_cont')
+            ->add('email_cont')
+            ->add('libelle_mess')
+            ->add('inscription_cont', CheckboxType::class, [
+                'label'    => 'inscrire au newsletter',
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+
+        ]);
+    }
+}
